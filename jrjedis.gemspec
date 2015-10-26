fc1fa46abaa954513bf61b59120f62805f66409a
Konstantin Shabanov <etehtsea@gmail.com>
2015-10-26 19:19:31 +0600

    Initial commit

---
+++ jrjedis.gemspec
@@ -0,0 +1,19 @@
+Gem::Specification.new do |spec|
+  spec.name          = "jrjedis"
+  spec.version       = "0.1.0"
+  spec.platform      = "java"
+  spec.authors       = ["Konstantin Shabanov"]
+  spec.email         = ["etehtsea@gmail.com"]
+
+  spec.summary       = spec.description = "Jedis wrapper"
+  spec.homepage      = "http://github.com/etehtsea/jrjedis"
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -- lib`.split("\n")
+  spec.test_files    = `git ls-files -- {spec}/*`.split("\n")
+
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "rake", ">= 10.0.0"
+  spec.add_development_dependency "pry"
+end
---
+++ lib/jrjedis.rb
@@ -0,0 +1,52 @@
+require 'java'
+require 'ext/jedis'
+require 'ext/commons-pool2'
+
+module JrJedis
+  class Pool
+    java_import Java::RedisClientsJedis::JedisPool
+    java_import Java::RedisClientsJedis::JedisPoolConfig
+
+    attr_reader :pool
+
+    # Initialize Jedis pool
+    # @param [Hash] opts Connection options
+    #
+    # @option options [String] :host ("localhost") Hostname or IP address.
+    # @option options [Integer] :port (6379) Redis server port.
+    # @option options [Integer] :timeout (5000) Connection timeout in ms.
+    # @option options [Integer] :min_idle Min idle threads in a pool.
+    # @option options [Integer] :max_idle Max idle threads in a pool.
+    # @option options [Integer] :max_total Max total active threads.
+    def initialize(opts = {})
+      host = opts.delete(:host) || 'localhost'
+      port = opts.delete(:port) || 6379
+      timeout = opts.delete(:timeout) || 5000
+
+      config = JedisPoolConfig.new
+      config.minIdle = opts.fetch(:min_idle) if opts[:min_idle]
+      config.maxIdle = opts.fetch(:max_idle) if opts[:max_idle]
+      config.maxTotal = opts.fetch(:max_total) if opts[:max_total]
+      config.block_when_exhausted = false
+      config.max_wait_millis = timeout
+      config.jmx_enabled = true
+
+      @pool = JedisPool.new(config, host, port, timeout)
+    end
+
+    def with
+      conn = @pool.resource
+      yield conn
+    ensure
+      conn.close if conn
+    end
+
+    def pipelined
+      with do |j|
+        pipeline = j.pipelined
+        response = yield pipeline
+        pipeline.sync_and_return_all.to_a
+      end
+    end
+  end
+end
